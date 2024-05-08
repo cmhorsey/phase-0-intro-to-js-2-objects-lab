a707fa027ffb2ee799e47d34ae45fe3228a36e94
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-05-08 09:10:10 -0400

    Create function to  update employee

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your solution in this file!
+const employee = {};
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+  const newObj = {...obj};
+
+  newObj[key] = value;
+
+  return newObj;
+};
+
+
